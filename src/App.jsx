501095e65cef747f01977008232ff151eb4df96f
haider-patanwala <haiderpatanwala@gmail.com>
2024-07-21 22:53:25 +0530

    feat: add react-router-dom and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {Button} from '@/components/ui/button'
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './Layout'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button  onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs text-7xl">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Layout />}
+          >
+            <Route
+              index
+              element={<Home />}
+            />
+            <Route
+              path="*"
+              element={<NotFound />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    )
+  }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+
+function NotFound() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <p className="text-5xl font-semibold">404</p>
+    </div>
+  )
+}
+
+export default NotFound
